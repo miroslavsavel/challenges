dfd83d2490edffcb9626cd1f3c169cec69a315c7
Miroslav Savel <miroslav.savel@gmail.com>
2022-03-29 14:51:05 +0200

    solution without conversion to string

---
+++ palindrom.py
@@ -0,0 +1,19 @@
+class Palindrom:
+    def __init__(self):
+        self.x = 10
+
+    def isNumPalindrom(self, x: int) -> bool:
+        if x < 0:
+            return (False)
+
+        rev_num = 0
+        digit = 0
+
+        while (x // (10 ** digit) != 0):
+            # strip digit and add to our reverse number
+            rev_num = (rev_num * 10) + (x // (10 ** digit) % 10)
+            # * 10 aby som posunul dane cislo na danu poziciu
+            # 73 * 10 + (737 // 100) % 10
+            digit += 1
+
+        return (x == rev_num)
